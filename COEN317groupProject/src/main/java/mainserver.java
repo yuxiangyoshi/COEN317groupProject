344ebd7e035649b71ef7dc2841ab7dbf32aed24b
Changyuan Zhang <zcy.pluto@gmail.com>
2018-03-09 17:14:50 -0800

    update

---
+++ COEN317groupProject/src/main/java/mainserver.java
@@ -0,0 +1,13 @@
+package main.java;
+
+/**
+ * Created by yuxia on 3/9/2018.
+ */
+
+import java.io.File;
+
+public class mainserver {
+    DBhandler dBhandle;
+    
+
+}
---
+++ COEN317groupProject/src/main/java/publisherHandler.java
@@ -0,0 +1,5 @@
+/**
+ * Created by yuxia on 3/5/2018.
+ */
+public class publisherHandler {
+}
